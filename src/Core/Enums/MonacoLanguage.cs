f9883d8cb6e7c5b071f3c67fd004ec2e98968c80
Johan <johan@anonymoos.com>
2024-11-23 21:40:00 -0500

    Add MonacoLanguage.cs enum

---
+++ src/Core/Enums/MonacoLanguage.cs
@@ -0,0 +1,16 @@
+﻿namespace Blazor.Monaco;
+
+
+public enum MonacoLanguage
+{
+    PlainText,
+    CSharp,
+    JavaScript,
+    PowerShell,
+    TypeScript,
+    HTML,
+    CSS,
+    JSON,
+    XML,
+    // Add more languages as needed
+}
