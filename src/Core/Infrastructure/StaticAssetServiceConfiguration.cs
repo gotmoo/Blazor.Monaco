4f281093e82921963e66652d3922de4b1c4505bc
Johan <johan@anonymoos.com>
2024-11-26 09:49:44 -0500

    Adding some copied infrastructure and utility classes from FluentUI Blazor

---
+++ src/Core/Infrastructure/StaticAssetServiceConfiguration.cs
@@ -0,0 +1,18 @@
+namespace Blazor.Monaco;
+
+public class StaticAssetServiceConfiguration
+{
+    private string _baseAddress = string.Empty;
+
+    internal event Action? OnUpdate;
+
+    public string BaseAddress
+    {
+        get => _baseAddress;
+        set
+        {
+            _baseAddress = value;
+            OnUpdate?.Invoke();
+        }
+    }
+}
