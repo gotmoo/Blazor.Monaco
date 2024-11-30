4df2219ece560dffb6980477b7e7b4ae4a8a4a57
Johan <johan@anonymoos.com>
2024-11-30 15:38:37 -0500

    Add MonacoLoaderUrl option to library configuration. Use this when loading the Monaco Loader in monacoInterop.cs

--- src/Core/GlobalState.cs
+++ src/Core/GlobalState.cs
@@ -9,36 +9,7 @@ namespace Blazor.Monaco;
 // TODO: #vNext: Rename this class to 'GlobalDesign' in the next major version.
 public class GlobalState
 {
-    public ElementReference Container { get; set; } = default!;
-    public StandardLuminance Luminance { get; set; } = StandardLuminance.LightMode;
     public bool InteropInDom { get; set; }
     public bool LoaderInDom { get; set; }
-    public IJSObjectReference JsObjectReference { get; set; } = default!;
     public DateTime? FirstLoadTime { get; set; }
-    public string? Color { get; set; }
-
-    public event Action? OnChange;
-
-    public void SetLuminance(StandardLuminance luminance)
-    {
-        Luminance = luminance;
-        NotifyStateHasChanged();
-    }
-
-    public void SetContainer(ElementReference container)
-    {
-        Container = container;
-        NotifyStateHasChanged();
-    }
-
-    public void SetColor(string? color)
-    {
-        Color = color;
-        NotifyStateHasChanged();
-    }
-
-    private void NotifyStateHasChanged()
-    {
-        OnChange?.Invoke();
-    }
 }
--- src/Core/Infrastructure/LibraryConfiguration.cs
+++ src/Core/Infrastructure/LibraryConfiguration.cs
@@ -15,24 +15,10 @@ public class LibraryConfiguration
     internal static readonly string? AssemblyVersion = typeof(LibraryConfiguration).Assembly.GetName().Version?.ToString();
 
     /// <summary>
-    /// Gets or sets a value indicating whether the library should use the TooltipServiceProvider.
-    /// If set to true, add the FluentTooltipProvider component at end of the MainLayout.razor page.
+    /// Gets or sets a the location of the Monaco Editor loader javascript file. The components will 
+    /// use this URL to initialize the editor.
     /// </summary>
-    public bool UseTooltipServiceProvider { get; set; } = true;
-
-    /// <summary>
-    /// Gets or sets the required label for the form fields.
-    /// </summary>
-    public MarkupString RequiredLabel { get; set; } = (MarkupString)
-        """
-        <span aria-label="required" aria-hidden="true" style="padding-inline-start: calc(var(--design-unit) * 1px); color: var(--error); pointer-events: none;">*</span>
-        """;
-
-    /// <summary>
-    /// Gets or sets the value indicating whether the library should close the tooltip if the cursor leaves the anchor and the tooltip.
-    /// By default, the tooltip closes if the cursor leaves the anchor, but not the tooltip itself.
-    /// </summary>
-    public bool HideTooltipOnCursorLeave { get; set; } = false;
+    public string MonacoLoaderUrl = "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.0/min/vs/loader.js";
 
     /// <summary>
     /// Gets or sets the function that formats the URL of the collocated JavaScript file,
