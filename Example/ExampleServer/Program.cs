b75420b61cf15a61b1a50eade5cee50ba12d6e97
Johan <johan@anonymoos.com>
2024-11-30 15:39:00 -0500

    Add example setting the MonacoLoaderUrl in server startup.

--- Example/ExampleServer/Program.cs
+++ Example/ExampleServer/Program.cs
@@ -6,7 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddBlazorMonacoComponents();
+builder.Services.AddBlazorMonacoComponents(config =>
+{
+    //Change this to specify your own CDN. Must be a full URL. 
+    config.MonacoLoaderUrl = "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.0/min/vs/loader.js";
+});
 
 var app = builder.Build();
 
